181576b8ad3e5d28dcf37a1b221316531f38d900
Praveen Kumar <praveen.kumar.tek@gmail.com>
2026-02-22 12:19:18 +0530

    vairable in js

---
+++ code/variable.js
@@ -0,0 +1,11 @@
+const messageText = "Welcome to Textio!";
+let isBirthday = true;
+
+if (isBirthday) {
+  const messageText = "Happy Birthday! ";
+  console.log("sending birthday message ....");
+  console.log("messageText", messageText);
+}
+
+console.log("sending welcome message:");
+console.log("messageText", messageText);
