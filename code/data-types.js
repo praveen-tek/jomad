11eca964220667cb634ede000475b40819550d67
Praveen Kumar <praveen.kumar.tek@gmail.com>
2026-02-22 12:14:08 +0530

    common datatypes

---
+++ code/data-types.js
@@ -0,0 +1,10 @@
+var smsSendingLimit = 20;
+var hasPermission = true;
+var username = "john_doe";
+var nothing;
+
+
+console.log("smsSendingLimit is a " + typeof smsSendingLimit);
+console.log("hasPermission is a " + typeof hasPermission);
+console.log("username is a " + typeof username);
+console.log("nothing is a " + typeof nothing);
