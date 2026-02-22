62ae3e8c796a6bd589e50efa46a2f86d56674558
Praveen Kumar <praveen.kumar.tek@gmail.com>
2026-02-22 17:25:11 +0530

    numbers in js

---
+++ code/number.js
@@ -0,0 +1,10 @@
+const promoMeassges = 12;
+const reminderMessages = 15;
+const welcomeMessages = 8;
+const supportMessages = 5;
+
+const totalMessagesSent = promoMeassges + reminderMessages + welcomeMessages + supportMessages;
+const averageMessages = totalMessagesSent / 4;
+
+console.log("Total messages: ", totalMessagesSent);
+console.log("Average messages: ", averageMessages)
