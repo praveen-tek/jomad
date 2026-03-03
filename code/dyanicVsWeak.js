e243d69d2ec83c7dd44ccbf41837363997a17075
Praveen Kumar <praveen.kumar.tek@gmail.com>
2026-03-03 11:21:54 +0530

    weak fix

---
+++ code/dyanicVsWeak.js
@@ -0,0 +1,10 @@
+let totalSentMessages = 100;
+// let totalReceivedMessages = "50";
+let totalReceivedMessages = 50;
+
+
+const totalMessages =  totalSentMessages + totalReceivedMessages;
+
+console.log("Total sent messages: ", totalSentMessages);
+console.log("Total recieved messages: ", totalReceivedMessages);
+console.log("Total messages: ", totalMessages);
