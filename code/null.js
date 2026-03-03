e1540203af7aa8fe33d96e4956a6765227107aa0
Praveen Kumar <praveen.kumar.tek@gmail.com>
2026-03-03 11:19:15 +0530

    null

---
+++ code/null.js
@@ -0,0 +1,8 @@
+let sentMessages = null;
+let deliveredMessages = null;
+let failedMessages = null;
+
+
+console.log("Sent is: ", sentMessages === null);
+console.log("Delivered is: ", deliveredMessages === null);
+console.log("Failed is:", failedMessages === null);
