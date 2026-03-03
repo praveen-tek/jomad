b2a954f6050e56c1221af03c2a9be8035f43d84f
Praveen Kumar <praveen.kumar.tek@gmail.com>
2026-03-03 11:17:03 +0530

    undeifiend

---
+++ code/undefined-undeclared.js
@@ -0,0 +1,10 @@
+let sentMessages;
+let deliveredMessages;
+let failedMessages;
+
+
+
+
+console.log("Sent is: ", sentMessages);
+console.log("Delivered is: ", deliveredMessages);
+console.log("Failed is:", failedMessages);
